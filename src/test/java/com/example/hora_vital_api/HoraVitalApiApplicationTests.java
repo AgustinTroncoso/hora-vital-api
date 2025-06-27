32f4c97b6f9446719e270048ac23b13a4e68ef0f
AGUSTIN IGNACIO TRONCOSO ROJAS <a.troncoso09@ufromail.cl>
2025-06-27 18:25:49 -0400

    Creacion de proyecto

---
+++ src/test/java/com/example/hora_vital_api/HoraVitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hora_vital_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraVitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
